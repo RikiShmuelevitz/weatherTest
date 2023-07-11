2cbc1e40b0e6fa2f581c920358d764ed937ce495
RikiShmuelevitz <116269801+RikiShmuelevitz@users.noreply.github.com>
2023-07-11 15:40:24 +0300

    Update WeatherForecastController.cs

--- WebApplication2/Controllers/WeatherForecastController.cs
+++ WebApplication2/Controllers/WeatherForecastController.cs
@@ -66,7 +66,98 @@ namespace WebApplication2.Controllers
 
             return res;
         }
-       
+       //links:
+       //react
+       //https://gist.github.com/roshanlabh/eac12ebf9fa76880699256aefcb8d955
+       //sql:
+       //https://github.com/Hardanish-Singh/CoderByte-Challenges-Solutions/blob/master/SQL/sql_vendor_join.sql
+      //https://github.com/Hardanish-Singh/CoderByte-Challenges-Solutions/blob/master/SQL/sql_member_count.sql
+      //csharp:
+      // https://coderbyte.com/solution/Tree%20Constructor
+      //json:
+      // https://dotnetfiddle.net/o5RPai
+      //kode in csharp:
+      
+      //longest:
+      //using System;
+     //using System.Text.RegularExpressions;
+     //class MainClass {
+ // public static string LongestWord(string sen) { 
+      
+  
+    // code goes here  
+    /* Note: In C# the return type of a function and the 
+       parameter types being passed are defined, so this return 
+       call must match the return type of the function.
+       You are free to modify the return type. */
+   // Regex reg = new Regex("[^a-zA-Z0-9]");
+   // sen = reg.Replace(sen, " ");
+
+
+   // string longestWord = String.Empty;
+  //  string[] arr = sen.Split(" ");
+  //  for (int i = 0; i < arr.Length; i++) {
+   //     if (arr[i].Length > longestWord.Length)
+   //         longestWord = arr[i];
+   // }
+  //  return longestWord;   
+//  }
+
+  //static void Main() {  
+    // keep this function call here
+  //  Console.WriteLine(LongestWord(Console.ReadLine()));
+ // } 
+   
+//}
+
+//aaabbc:
+//using System;
+//using System.Linq;
+//using System.Text.RegularExpressions;
+//class MainClass {
+  //aabbbc
+//  public static string LongestWord(string str) { 
+   // string newStr="";
+   // char prev=str[0];
+   // int count=1;
+    //for(int i=1;i<str.Length;i++){
+    
+
+    //if(str[i]!=prev){
+    // newStr+=count;
+     //    newStr+=prev;
+  //   count=1;
+
+   //     }
+   //     else
+    //    count++;
+    
+     //   prev=str[i];
+     //     if(i==str.Length-1){
+     //         newStr+=count;
+     //    newStr+=prev;
+     //     }
+         
+      //  }
+
+  //  return newStr;   
+ // }
+
+ // static void Main() {  
+    // keep this function call here
+  //  Console.WriteLine(LongestWord(Console.ReadLine()));
+ // } 
+   
+//}
+
+
+
+
+
+
+
+      
+
 
     }
 }
